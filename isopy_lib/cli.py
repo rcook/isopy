1b6f12a913f8b02b724e78cca131f84ea9c78e11
Richard Cook <rcook@rcook.org>
2023-03-11 00:09:39 -0800

    Set default log level

--- isopy_lib/cli.py
+++ isopy_lib/cli.py
@@ -48,4 +48,5 @@ def add_log_level_arg(parser):
         metavar="LOG_LEVEL",
         choices=choices,
         type=str,
+        default="info",
         help=f"logging level (one of {', '.join(choices)})")
