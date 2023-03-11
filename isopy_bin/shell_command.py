fb29252566c55a959768fae5c3c72655c9da1431
Richard Cook <rcook@rcook.org>
2023-03-11 00:54:36 -0800

    Use colour

--- isopy_bin/shell_command.py
+++ isopy_bin/shell_command.py
@@ -1,11 +1,16 @@
 from isopy_lib.env import exec_environment
+from isopy_lib.xprint import xprint
+import colorama
 import os
 
 
 def do_shell(ctx, env):
     with exec_environment(ctx=ctx, env=env) as (python_bin_dir, e):
-        print(
+        xprint(
+            colorama.Fore.LIGHTYELLOW_EX,
             f"Python shell for environment {env}; Python is at {python_bin_dir}")
-        print(f"Type \"exit\" to return to parent shell")
+        xprint(
+            colorama.Fore.YELLOW,
+            "Type \"exit\" to return to parent shell")
         shell = os.getenv("SHELL")
         os.execlpe(shell, shell, e)
