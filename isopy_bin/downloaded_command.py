3b30f3c5da768d840e3c8a7e01d3e9c89fe63dd6
Richard Cook <rcook@rcook.org>
2023-03-11 01:31:53 -0800

    Show file size

--- isopy_bin/downloaded_command.py
+++ isopy_bin/downloaded_command.py
@@ -1,4 +1,5 @@
 from isopy_lib.asset import assets_dir as __assets_dir
+from isopy_lib.fs import file_path
 from isopy_lib.version import Version
 from isopy_lib.xprint import xprint
 from operator import itemgetter
@@ -13,14 +14,15 @@ CPYTHON_PREFIX_LEN = len(CPYTHON_PREFIX)
 def do_downloaded(ctx):
     assets_dir = __assets_dir(ctx.cache_dir)
     if os.path.isdir(assets_dir):
-        ps = []
+        items = []
         for f in os.listdir(assets_dir):
             if f.startswith(CPYTHON_PREFIX):
                 idx = f.index("+", CPYTHON_PREFIX_LEN)
                 version = Version.parse(f[CPYTHON_PREFIX_LEN:idx])
-                ps.append((f, version))
+                p = file_path(assets_dir, f)
+                items.append((f, version, os.path.getsize(p)))
 
-        for f, version in sorted(ps, key=itemgetter(1), reverse=True):
-            xprint(colorama.Fore.YELLOW, f, " ", version)
+        for f, version, size in sorted(items, key=itemgetter(1), reverse=True):
+            xprint(colorama.Fore.YELLOW, f, " ", version, " ", f"{size} bytes")
     else:
         print("There are no downloads yet!")
