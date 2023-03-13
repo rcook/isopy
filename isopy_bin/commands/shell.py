06d8bd335bafaa0637ee57e1fe49921079baa102
Richard Cook <rcook@rcook.org>
2023-03-12 17:49:43 -0700

    Use environment variable to prevent nested shells

--- isopy_bin/commands/shell.py
+++ isopy_bin/commands/shell.py
@@ -1,16 +1,27 @@
 from isopy_lib.env import get_env_config
+from isopy_lib.errors import ReportableError
 from isopy_lib.xprint import xprint
 import colorama
 import os
 
 
+ISOPY_ENV_VAR_NAME = "ISOPY_ENV"
+
+
 def do_shell(ctx, env):
+    if os.getenv(ISOPY_ENV_VAR_NAME) is not None:
+        raise ReportableError(
+            "You are already in an active isopy environment")
+
     env_config = get_env_config(ctx=ctx, env=env)
+
+    env_identifier = env_config.name or env_config.dir_config_path
     xprint(
         colorama.Fore.LIGHTYELLOW_EX,
-        f"Python shell for environment {env_config.name or env_config.dir_config_path}")
+        f"Python shell for environment {env_identifier}")
     xprint(
         colorama.Fore.YELLOW,
         "Type \"exit\" to return to parent shell")
 
+    os.environ[ISOPY_ENV_VAR_NAME] = env_identifier
     env_config.execlpe(os.getenv("SHELL"), ctx=ctx)
