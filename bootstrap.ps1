a7192aa4b70077f0f84cbcf4025bc5d6f1d176e8
Richard Cook <rcook@rcook.org>
2023-03-13 17:48:23 -0700

    Partial implementation of PowerShell bootstrap script

---
+++ bootstrap.ps1
@@ -0,0 +1,57 @@
+#Requires -Version 5
+[CmdletBinding()]
+param()
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+function New-TemporaryDirectory {
+    $tempDir = [System.IO.Path]::GetTempPath()
+    [string] $name = [System.Guid]::NewGuid()
+    New-Item -ItemType Directory -Path (Join-Path -Path $tempDir -ChildPath $name)
+}
+
+
+$cacheDir = Join-Path -Path $env:USERPROFILE -ChildPath .isopy
+$pythonVersion = '3.11.1'
+$tag = '20230116'
+
+$assetsDir = Join-Path -Path $cacheDir -ChildPath assets
+$isopyEnvDir = Join-Path -Path $cacheDir -ChildPath envs\isopy
+$isopyEnvPath = Join-Path -Path $isopyEnvDir -ChildPath env.yaml
+$pythonBaseName = "cpython-$pythonVersion+$tag"
+$isopyPythonDir = Join-Path -Path $isopyEnvDir -ChildPath python
+$binDir = Join-Path -Path $env:USERPROFILE -Child .local\bin
+
+$pythonFileName = "$pythonBaseName-x86_64-pc-windows-msvc-shared-install_only.tar.gz"
+
+$pythonUrl = "https://github.com/indygreg/python-build-standalone/releases/download/$tag/$pythonFileName"
+$pythonPath= Join-Path -Path $assetsDir -ChildPath $pythonFileName
+
+
+New-Item -ItemType Directory -Path $assetsDir -Force | Out-Null
+New-Item -ItemType Directory -Path $isopyPythonDir -Force | Out-Null
+
+if (-not (Test-Path -Path $pythonPath)) {
+    Invoke-WebRequest -Uri $pythonUrl -OutFile $pythonPath
+    $hash = (Get-FileHash -Path $pythonPath -Algorithm SHA256).Hash
+    $expectedHash = Get-Content -Path $PSScriptRoot\sha256sums\$pythonFileName.sha256
+    if ($hash -ne $expectedHash) {
+        Remove-Item -Recurse -Force -Path $pythonPath
+        throw "Checksum failed on $pythonPath"
+    }
+}
+
+if (-not (Test-Path -Path $isopyPythonDir\python3.exe)) {
+    $d = New-TemporaryDirectory
+    try {
+        & tar xf $pythonPath -C $d.FullName
+        $sourceDir = Join-Path -Path $d -ChildPath python
+        Move-Item -Path $sourceDir\* $isopyPythonDir
+    }
+    finally {
+        if ($null -ne $d) {
+            Remove-Item -Recurse -Force -Path $d
+        }
+    }
+}
