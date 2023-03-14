a432c73a9897f3706d7eaa77df39fefc181d134b
Richard Cook <rcook@rcook.org>
2023-03-13 20:03:52 -0700

    Fix wrapper script

--- isopy.ps1
+++ isopy.ps1
@@ -7,16 +7,16 @@ param(
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
-$pythonDir = 'C:\Users\rcook\.isopy\hashed\7f0ec6a91047ee431ff45f55a6914c9c\cpython-3.11.1+20230116'
+$pythonDir = Join-Path -Path $env:USERPROFILE -ChildPath .isopy\envs\isopy\python
 $thisDir = $PSScriptRoot
 try {
-    $tempPath = $env:PATH
+    $tempPath = $env:Path
     $tempPythonPath = $env:PYTHONPATH
-    $env:PATH = $pythonDir + ';' + $env:PATH
+    $env:PATH = $pythonDir + ';' + "$pythonDir\Scripts" + ';' + $env:Path
     $env:PYTHONPATH = $thisDir
-    & python.exe $thisDir\isopy_bin\main.py $Arguments
+    & python $thisDir\isopy_bin\main.py $Arguments
 }
 finally {
     $env:PYTHONPATH = $tempPythonPath
-    $env:PATH = $tempPath
+    $env:Path = $tempPath
 }
