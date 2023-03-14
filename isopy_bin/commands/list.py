a55b488693a4fcfaebcbba8fe8cad53403cf4358
Richard Cook <rcook@rcook.org>
2023-03-13 20:07:18 -0700

    Fix list command

--- isopy_bin/commands/list.py
+++ isopy_bin/commands/list.py
@@ -13,10 +13,10 @@ def transform(env_config, detailed):
             return PLATFORM.home_dir_meta + s[len(PLATFORM.home_dir):]
         return s
 
+    name_or_dir = truncate_path(env_config.dir_config_path) \
+        if env_config.name is None \
+        else env_config.name
     if detailed:
-        name_or_dir = truncate_path(env_config.dir_config_path) \
-            if env_config.name is None \
-            else env_config.name
         return {
             "path": truncate_path(env_config.path),
             "name_or_dir": name_or_dir,
@@ -25,10 +25,11 @@ def transform(env_config, detailed):
             "PATH": PLATFORM.path_env(env_config=env_config)
         }
     else:
+
         return {
-            "name_or_dir": truncate_path(x.dir_config_path) if x.name is None else x.name,
-            "tag": x.tag,
-            "ver": x.python_version
+            "name_or_dir": name_or_dir,
+            "tag": env_config.tag,
+            "ver": env_config.python_version
         }
 
 
