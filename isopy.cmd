2a8521d8e609b14cfee103df1391a5c37f5aff43
Richard Cook <rcook@rcook.org>
2023-03-14 20:07:37 -0700

    Add quotes

--- isopy.cmd
+++ isopy.cmd
@@ -2,4 +2,4 @@
 setlocal
 set PATH=%USERPROFILE%\.isopy\envs\isopy\python;%USERPROFILE%\.isopy\envs\isopy\python\Scripts;%PATH%
 set PYTHONPATH=%~dp0
-python %~dp0isopy_bin\main.py %*
+python "%~dp0isopy_bin\main.py" %*
