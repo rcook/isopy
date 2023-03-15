3b0360f12cda77a6586dc410d235a6e7fca25817
Richard Cook <rcook@rcook.org>
2023-03-15 04:25:06 -0700

    Get rid of module wrapper

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -1,15 +1,15 @@
-New-Module -Name isopysetup -ScriptBlock {
-  function setup() {
-    param(
-      [Parameter(Mandatory = $false, Position = 0, ValueFromRemainingArguments = $true)]
-      [object[]] $Arguments
-    )
+#Requires -Version 5
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory = $false, Position = 0, ValueFromRemainingArguments = $true)]
+  [object[]] $Arguments
+)
 
-    Set-StrictMode -Version Latest
-    $ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
 
-    function Show-Usage {
-      Write-Host -Object @"
+function Show-Usage {
+  Write-Host -Object @"
 Usage: setup [-h] [--force] [--stdout] [SCRIPT_PATH]
 
   SCRIPT_PATH  path to wrapper script
@@ -18,159 +18,159 @@ Usage: setup [-h] [--force] [--stdout] [SCRIPT_PATH]
 
 Visit https://github.com/rcook/isopy for more information
 "@
-    }
+}
 
-    function New-TemporaryDirectory {
-      $tempDir = [System.IO.Path]::GetTempPath()
-      [string] $name = [System.Guid]::NewGuid()
-      New-Item -ItemType Directory -Path (Join-Path -Path $tempDir -ChildPath $name)
-    }
+function New-TemporaryDirectory {
+  $tempDir = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  New-Item -ItemType Directory -Path (Join-Path -Path $tempDir -ChildPath $name)
+}
 
 
-    $argv = @() + $Arguments
-    if (($argv.Length -eq 1) -and ($null -eq $argv[0])) {
+$argv = @() + $Arguments
+if (($argv.Length -eq 1) -and ($null -eq $argv[0])) {
+  Show-Usage
+  exit 1
+}
+
+if ($argv -in @('-h', '--help')) {
+  Show-Usage
+  exit 0
+}
+
+$scriptPath = $null
+$force = $null
+$stdout = $null
+while ($null -ne $argv) {
+  [string] $arg, [string[]] $argv = $argv
+  if ($arg.Length -eq 0) { break }
+
+  if ($arg -in @('-f', '--force')) {
+    if ($null -ne $force) {
+      Write-Host -Object "--force already specified`n"
       Show-Usage
       exit 1
     }
+    $force = $true
+    continue
+  }
 
-    if ($argv -in @('-h', '--help')) {
+  if ($arg -in @('-o', '--stdout')) {
+    if ($null -ne $stdout) {
+      Write-Host -Object "--stdout already specified`n"
       Show-Usage
-      exit 0
-    }
-
-    $scriptPath = $null
-    $force = $null
-    $stdout = $null
-    while ($null -ne $argv) {
-      [string] $arg, [string[]] $argv = $argv
-      if ($arg.Length -eq 0) { break }
-
-      if ($arg -in @('-f', '--force')) {
-        if ($null -ne $force) {
-          Write-Host -Object "--force already specified`n"
-          Show-Usage
-          exit 1
-        }
-        $force = $true
-        continue
-      }
-
-      if ($arg -in @('-o', '--stdout')) {
-        if ($null -ne $stdout) {
-          Write-Host -Object "--stdout already specified`n"
-          Show-Usage
-          exit 1
-        }
-        $stdout = $true
-        continue
-      }
-
-      if ($arg.StartsWith('-')) {
-        Write-Host -Object "Invalid option $arg`n"
-        Show-Usage
-        exit 1
-      }
-
-      if ($null -ne $scriptPath) {
-        Write-Host -Object "Script path already specified`n"
-        Show-Usage
-        exit 1
-      }
-
-      [string] $scriptPath = Resolve-Path -Path $arg -ErrorAction SilentlyContinue -ErrorVariable _frperror
-      if (-not $scriptPath) {
-        $scriptPath = $_frperror[0].TargetObject
-      }
-    }
-
-    if ((-not $force) -and ($null -ne $scriptPath) -and (Test-Path -Path $scriptPath)) {
-      Write-Host -Object "Script $scriptPath already exists; specify --force to overwrite it"
       exit 1
     }
+    $stdout = $true
+    continue
+  }
 
-    $isopyBranch = 'main'
-    $isopyUrl = "https://github.com/rcook/isopy/archive/refs/heads/$isopyBranch.zip"
-    $isopyDir = Join-Path -Path $env:USERPROFILE -ChildPath .isopy
-    $isopySrcDir = Join-Path -Path $isopyDir -ChildPath src
-    $isopyAssetsDir = Join-Path -Path $isopyDir -ChildPath assets
-    $isopyBranchDir = Join-Path -Path $isopySrcDir -ChildPath $isopyBranch
-    $isopyEnvDir = Join-Path -Path $isopyDir -ChildPath envs\isopy
-    $isopyEnvPath = Join-Path -Path $isopyEnvDir -ChildPath env.yaml
-    $isopyEnvPythonDir = Join-Path -Path $isopyEnvDir -ChildPath python
-    $isopyEnvScriptsDir = Join-Path -Path $isopyEnvPythonDir -ChildPath Scripts
+  if ($arg.StartsWith('-')) {
+    Write-Host -Object "Invalid option $arg`n"
+    Show-Usage
+    exit 1
+  }
 
-    $tempDir = New-TemporaryDirectory
-    if (-not (Test-Path -Path $isopyBranchDir)) {
-      try {
-        $tempZipDir = Join-Path -Path $tempDir -ChildPath isopy-$isopyBranch
-        $tempZipPath = Join-Path -Path $tempDir -ChildPath isopy-$isopyBranch.zip
-        Invoke-WebRequest -Uri $isopyUrl -OutFile $tempZipPath
-        Expand-Archive -Path $tempZipPath -DestinationPath $tempDir
-        New-Item -ItemType Directory -Path $isopySrcDir -Force | Out-Null
-        $temp = Join-Path -Path $tempDir -ChildPath $isopyBranch
-        Rename-Item -Path $tempZipDir -NewName $temp
-        Move-Item -Path $temp -Destination $isopySrcDir
-      }
-      finally {
-        if ($null -ne $tempDir) {
-          Remove-Item -Recurse -Force -Path $tempDir
-        }
-      }
+  if ($null -ne $scriptPath) {
+    Write-Host -Object "Script path already specified`n"
+    Show-Usage
+    exit 1
+  }
+
+  [string] $scriptPath = Resolve-Path -Path $arg -ErrorAction SilentlyContinue -ErrorVariable _frperror
+  if (-not $scriptPath) {
+    $scriptPath = $_frperror[0].TargetObject
+  }
+}
+
+if ((-not $force) -and ($null -ne $scriptPath) -and (Test-Path -Path $scriptPath)) {
+  Write-Host -Object "Script $scriptPath already exists; specify --force to overwrite it"
+  exit 1
+}
+
+$isopyBranch = 'main'
+$isopyUrl = "https://github.com/rcook/isopy/archive/refs/heads/$isopyBranch.zip"
+$isopyDir = Join-Path -Path $env:USERPROFILE -ChildPath .isopy
+$isopySrcDir = Join-Path -Path $isopyDir -ChildPath src
+$isopyAssetsDir = Join-Path -Path $isopyDir -ChildPath assets
+$isopyBranchDir = Join-Path -Path $isopySrcDir -ChildPath $isopyBranch
+$isopyEnvDir = Join-Path -Path $isopyDir -ChildPath envs\isopy
+$isopyEnvPath = Join-Path -Path $isopyEnvDir -ChildPath env.yaml
+$isopyEnvPythonDir = Join-Path -Path $isopyEnvDir -ChildPath python
+$isopyEnvScriptsDir = Join-Path -Path $isopyEnvPythonDir -ChildPath Scripts
+
+$tempDir = New-TemporaryDirectory
+if (-not (Test-Path -Path $isopyBranchDir)) {
+  try {
+    $tempZipDir = Join-Path -Path $tempDir -ChildPath isopy-$isopyBranch
+    $tempZipPath = Join-Path -Path $tempDir -ChildPath isopy-$isopyBranch.zip
+    Invoke-WebRequest -Uri $isopyUrl -OutFile $tempZipPath
+    Expand-Archive -Path $tempZipPath -DestinationPath $tempDir
+    New-Item -ItemType Directory -Path $isopySrcDir -Force | Out-Null
+    $temp = Join-Path -Path $tempDir -ChildPath $isopyBranch
+    Rename-Item -Path $tempZipDir -NewName $temp
+    Move-Item -Path $temp -Destination $isopySrcDir
+  }
+  finally {
+    if ($null -ne $tempDir) {
+      Remove-Item -Recurse -Force -Path $tempDir
     }
+  }
+}
 
-    $yamlPath = Join-Path -Path $isopyBranchDir -ChildPath .isopy.yaml
-    $c = (Get-Content -Path $yamlPath) -join ' '
-    $m = [regex]::Match($c, "python_version: '?([^\s']+)'?")
-    if (-not $m.Success) { throw 'Regex match failed' }
-    $pythonVersion = $m.Groups[1].Value
-    $m = [regex]::Match($c, "tag: '?([^\s']+)'?")
-    if (-not $m.Success) { throw 'Regex match failed' }
-    $tag = $m.Groups[1].Value
-    $pythonBaseName = "cpython-$pythonVersion+$tag"
-    $pythonFileName = "$pythonBaseName-x86_64-pc-windows-msvc-shared-install_only.tar.gz"
+$yamlPath = Join-Path -Path $isopyBranchDir -ChildPath .isopy.yaml
+$c = (Get-Content -Path $yamlPath) -join ' '
+$m = [regex]::Match($c, "python_version: '?([^\s']+)'?")
+if (-not $m.Success) { throw 'Regex match failed' }
+$pythonVersion = $m.Groups[1].Value
+$m = [regex]::Match($c, "tag: '?([^\s']+)'?")
+if (-not $m.Success) { throw 'Regex match failed' }
+$tag = $m.Groups[1].Value
+$pythonBaseName = "cpython-$pythonVersion+$tag"
+$pythonFileName = "$pythonBaseName-x86_64-pc-windows-msvc-shared-install_only.tar.gz"
 
-    $pythonUrl = "https://github.com/indygreg/python-build-standalone/releases/download/$tag/$pythonFileName"
-    $pythonPath = Join-Path -Path $isopyAssetsDir -ChildPath $pythonFileName
+$pythonUrl = "https://github.com/indygreg/python-build-standalone/releases/download/$tag/$pythonFileName"
+$pythonPath = Join-Path -Path $isopyAssetsDir -ChildPath $pythonFileName
 
-    New-Item -ItemType Directory -Path $isopyAssetsDir -Force | Out-Null
-    New-Item -ItemType Directory -Path $isopyEnvPythonDir -Force | Out-Null
+New-Item -ItemType Directory -Path $isopyAssetsDir -Force | Out-Null
+New-Item -ItemType Directory -Path $isopyEnvPythonDir -Force | Out-Null
 
-    if (-not (Test-Path -Path $pythonPath)) {
-      Invoke-WebRequest -Uri $pythonUrl -OutFile $pythonPath
-      $hash = (Get-FileHash -Path $pythonPath -Algorithm SHA256).Hash
-      $expectedHash = Get-Content -Path (Join-Path -Path $isopyBranchDir -ChildPath sha256sums\$pythonFileName.sha256)
-      if ($hash -ne $expectedHash) {
-        Remove-Item -Recurse -Force -Path $pythonPath
-        Write-Host -Object "Checksum failed on $pythonPath"
-        exit 1
-      }
+if (-not (Test-Path -Path $pythonPath)) {
+  Invoke-WebRequest -Uri $pythonUrl -OutFile $pythonPath
+  $hash = (Get-FileHash -Path $pythonPath -Algorithm SHA256).Hash
+  $expectedHash = Get-Content -Path (Join-Path -Path $isopyBranchDir -ChildPath sha256sums\$pythonFileName.sha256)
+  if ($hash -ne $expectedHash) {
+    Remove-Item -Recurse -Force -Path $pythonPath
+    Write-Host -Object "Checksum failed on $pythonPath"
+    exit 1
+  }
+}
+
+if (-not (Test-Path -Path $isopyEnvPythonDir\python.exe)) {
+  $d = New-TemporaryDirectory
+  try {
+    & tar xf $pythonPath -C $d.FullName
+    $sourceDir = Join-Path -Path $d -ChildPath python
+    Move-Item -Path $sourceDir\* -Destination $isopyEnvPythonDir
+  }
+  finally {
+    if ($null -ne $d) {
+      Remove-Item -Recurse -Force -Path $d
     }
+  }
 
-    if (-not (Test-Path -Path $isopyEnvPythonDir\python.exe)) {
-      $d = New-TemporaryDirectory
-      try {
-        & tar xf $pythonPath -C $d.FullName
-        $sourceDir = Join-Path -Path $d -ChildPath python
-        Move-Item -Path $sourceDir\* -Destination $isopyEnvPythonDir
-      }
-      finally {
-        if ($null -ne $d) {
-          Remove-Item -Recurse -Force -Path $d
-        }
-      }
+  try {
+    $tempPath = $env:PATH
+    $env:Path = $isopyEnvPythonDir + ';' + $isopyEnvScriptsDir + ';' + $env:Path
+    & python -m pip install --upgrade pip
+    & python -m pip install -r (Join-Path -Path $isopyBranchDir -ChildPath requirements.txt)
+  }
+  finally {
+    $env:Path = $tempPath
+  }
+}
 
-      try {
-        $tempPath = $env:PATH
-        $env:Path = $isopyEnvPythonDir + ';' + $isopyEnvScriptsDir + ';' + $env:Path
-        & python -m pip install --upgrade pip
-        & python -m pip install -r (Join-Path -Path $isopyBranchDir -ChildPath requirements.txt)
-      }
-      finally {
-        $env:Path = $tempPath
-      }
-    }
-
-    Set-Content -Path $isopyEnvPath -Value (@"
+Set-Content -Path $isopyEnvPath -Value (@"
 name: isopy
 python_dir: python
 python_version: $pythonVersion
@@ -178,7 +178,7 @@ tag: '$tag'
 "@)
 
 
-    $wrapper = @"
+$wrapper = @"
 Set-Content -Path foo.cmd -Value @"
 @echo off
 setlocal
@@ -188,14 +188,14 @@ python "$isopyBranchDir\isopy_bin\main.py" %*
 `"@
 "@
 
-    if ($stdout) {
-      Write-Host -Object @"
+if ($stdout) {
+  Write-Host -Object @"
 --------------------------------------------------
 Create a Windows command file on PATH containing the following
 --------------------------------------------------
 "@
-      Write-Host -Object $wrapper
-      Write-Host -Object @"
+  Write-Host -Object $wrapper
+  Write-Host -Object @"
 --------------------------------------------------
 
 --------------------------------------------------
@@ -206,11 +206,8 @@ Or stick this at the top of your PowerShell profile file:
     `$env:Path
 --------------------------------------------------
 "@
-    }
-    else {
-      Set-Content -Path $scriptPath -Value $wrapper
-      Write-Host -Object "Wrapper script generated at $scriptPath; please make sure this file is on PATH"
-    }
-  }
-  Export-ModuleMember -Function setup
+}
+else {
+  Set-Content -Path $scriptPath -Value $wrapper
+  Write-Host -Object "Wrapper script generated at $scriptPath; please make sure this file is on PATH"
 }
