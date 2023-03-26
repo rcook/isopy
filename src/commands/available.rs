897684e7da9bc795a9aad75879c2138fdde67ca7
Richard Cook <rcook@rcook.org>
2023-03-26 12:57:48 -0700

    Dump out asset info

--- src/commands/available.rs
+++ src/commands/available.rs
@@ -1,7 +1,17 @@
 use crate::config::Config;
 use crate::error::Result;
+use crate::object_model::AssetInfo;
+use std::fs::read_dir;
 
-pub fn do_available(_config:&Config) -> Result<()> {
-    println!("Hello World");
+pub fn do_available(config: &Config) -> Result<()> {
+    let assets_dir = config.dir.join("assets");
+    for e in read_dir(assets_dir)? {
+        let temp = e?;
+        if let Some(asset_info) =
+            AssetInfo::from_asset_name(temp.file_name().to_str().expect("Must be a valid string"))
+        {
+            println!("asset_info={:?}", asset_info)
+        }
+    }
     Ok(())
 }
