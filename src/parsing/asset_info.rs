39dd178417b32e95aaf9382266a29f45a22e5fc8
Richard Cook <rcook@rcook.org>
2023-03-25 22:11:24 -0700

    Handle tags better

--- src/parsing/asset_info.rs
+++ src/parsing/asset_info.rs
@@ -1,9 +1,12 @@
 use crate::version::Version;
+use std::collections::{hash_set, HashSet};
 
-const SUPPORTED_EXTS: [&str; 2] = [".tar.gz", ".tar.zst"];
+const EXTS: [&str; 2] = [".tar.gz", ".tar.zst"];
+const SUBFLAVOURS: [&str; 1] = ["full"]; // Should be a set!
 
 #[derive(Debug, PartialEq)]
 pub struct AssetInfo {
+    name: String,
     family: Family,
     version: Version,
     tag: Tag,
@@ -28,15 +31,9 @@ impl Family {
 }
 
 #[derive(Debug, PartialEq)]
-struct Tag(String);
-
-impl Tag {
-    fn from_str<S>(s: S) -> Self
-    where
-        S: Into<String>,
-    {
-        Self(s.into())
-    }
+enum Tag {
+    NewStyle(String),
+    OldStyle(String),
 }
 
 #[derive(Debug, PartialEq)]
@@ -116,7 +113,7 @@ impl AssetInfo {
     pub fn from_asset_name(s: &str) -> Option<Self> {
         let mut ext = None::<Ext>;
         let mut ext_len = 0;
-        for e in SUPPORTED_EXTS {
+        for e in EXTS {
             if (s.ends_with(e)) {
                 ext = Some(Ext::from_str(e));
                 ext_len = e.len();
@@ -128,22 +125,22 @@ impl AssetInfo {
         }
 
         let ext = ext.unwrap();
-        let s = &s[..s.len() - ext_len];
+        let base_name = &s[..s.len() - ext_len];
 
-        let mut iter = s.split("-").into_iter();
+        let mut iter = base_name.split("-").into_iter();
 
         let family = Family::from_str(iter.next()?);
 
         let version_tag = iter.next()?;
 
         let parts = version_tag.split("+").collect::<Vec<_>>();
-        if parts.len() != 2 {
-            return None;
-        }
+        let (version_str, mut tag) = match parts.len() {
+            1 => (parts[0], None),
+            2 => (parts[0], Some(Tag::NewStyle(String::from(parts[1])))),
+            _ => return None,
+        };
 
-        let version = Version::parse(parts[0])?;
-
-        let tag = Tag::from_str(parts[1]);
+        let version = Version::parse(version_str)?;
 
         let arch = Arch::from_str(iter.next()?);
 
@@ -153,12 +150,29 @@ impl AssetInfo {
 
         let flavour = Flavour::from_str(iter.next()?);
 
-        let subflavour = iter.next().map(Subflavour::from_str);
+        let (subflavour, tag) = match iter.next() {
+            Some(temp) => {
+                let mut sf_temp = None::<Subflavour>;
+                for sf in SUBFLAVOURS {
+                    if sf == temp {
+                        sf_temp = Some(Subflavour::from_str(temp))
+                    }
+                }
+
+                if sf_temp.is_none() {
+                    (None, Some(Tag::OldStyle(String::from(temp))))
+                } else {
+                    (sf_temp, tag)
+                }
+            }
+            None => (None, tag),
+        };
 
         Some(Self {
+            name: String::from(s),
             family: family,
             version: version,
-            tag: tag,
+            tag: tag.expect("Must be set"),
             arch: arch,
             platform: platform,
             os: os,
@@ -178,9 +192,10 @@ mod tests {
     #[rstest]
     #[case(
         AssetInfo {
+            name: String::from("cpython-3.10.9+20230116-aarch64-apple-darwin-debug-full.tar.zst"),
             family: Family::from_str("cpython"),
             version: Version::parse("3.10.9").expect("Should parse"),
-            tag: Tag::from_str("20230116"),
+            tag: Tag::NewStyle(String::from("20230116")),
             arch: Arch::from_str("aarch64"),
             platform: Platform::from_str("apple"),
             os: OS::from_str("darwin"),
@@ -192,9 +207,10 @@ mod tests {
     )]
     #[case(
         AssetInfo {
+            name: String::from("cpython-3.10.9+20230116-aarch64-apple-darwin-install_only.tar.gz"),
             family: Family::from_str("cpython"),
             version: Version::parse("3.10.9").expect("Should parse"),
-            tag: Tag::from_str("20230116"),
+            tag: Tag::NewStyle(String::from("20230116")),
             arch: Arch::from_str("aarch64"),
             platform: Platform::from_str("apple"),
             os: OS::from_str("darwin"),
@@ -203,6 +219,21 @@ mod tests {
             ext: Ext::from_str(".tar.gz"),
         },
         "cpython-3.10.9+20230116-aarch64-apple-darwin-install_only.tar.gz"
+    )]
+    #[case(
+        AssetInfo {
+            name: String::from("cpython-3.10.2-aarch64-apple-darwin-debug-20220220T1113.tar.zst"),
+            family: Family::from_str("cpython"),
+            version: Version::parse("3.10.2").expect("Should parse"),
+            tag: Tag::OldStyle(String::from("20220220T1113")),
+            arch: Arch::from_str("aarch64"),
+            platform: Platform::from_str("apple"),
+            os: OS::from_str("darwin"),
+            flavour: Flavour::from_str("debug"),
+            subflavour: None,
+            ext: Ext::from_str(".tar.zst"),
+        },
+        "cpython-3.10.2-aarch64-apple-darwin-debug-20220220T1113.tar.zst"
     )]
     fn test_from_asset_name(#[case] expected_result: AssetInfo, #[case] input: &str) {
         assert_eq!(Some(expected_result), AssetInfo::from_asset_name(input))
