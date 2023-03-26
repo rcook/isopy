d9720b90e0fc633125f289305b3dfb5ea5581360
Richard Cook <rcook@rcook.org>
2023-03-26 11:04:58 -0700

    Change use statement

--- src/object_model/mod.rs
+++ src/object_model/mod.rs
@@ -2,12 +2,6 @@ mod asset_info;
 mod attributes;
 
 pub use self::asset_info::AssetInfo;
-pub use self::attributes::Arch;
-pub use self::attributes::ArchiveType;
-pub use self::attributes::Family;
-pub use self::attributes::Flavour;
-pub use self::attributes::Platform;
-pub use self::attributes::Subflavour;
-pub use self::attributes::Tag;
-pub use self::attributes::Variant;
-pub use self::attributes::OS;
+pub use self::attributes::{
+    Arch, ArchiveType, Family, Flavour, Platform, Subflavour, Tag, Variant, OS,
+};
