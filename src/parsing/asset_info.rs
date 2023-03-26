d70f8f3cc1391a9b49077c9e6dac56a2a2a0ff91
Richard Cook <rcook@rcook.org>
2023-03-25 20:09:51 -0700

    Parse more fields

--- src/parsing/asset_info.rs
+++ src/parsing/asset_info.rs
@@ -1,10 +1,38 @@
-struct AssetInfo {}
+#[derive(Debug, PartialEq)]
+struct AssetInfo {
+    family: String,
+    version: String,
+    tag: String,
+}
 
 #[allow(unused)]
 impl AssetInfo {
-    fn from_asset_name(s: &str) -> bool {
-        let temp = s.split(".");
-        false
+    fn from_asset_name(s: &str) -> Option<Self> {
+        let mut iter = s.split("-").into_iter();
+
+        let family = match iter.next() {
+            Some(x) => x,
+            None => return None,
+        };
+
+        let version_tag = match iter.next() {
+            Some(x) => x,
+            None => return None,
+        };
+
+        let parts = version_tag.split("+").collect::<Vec<_>>();
+        if parts.len() != 2 {
+            return None;
+        }
+
+        let version = parts[0];
+        let tag = parts[1];
+
+        Some(Self {
+            family: String::from(family),
+            version: String::from(version),
+            tag: String::from(tag),
+        })
     }
 }
 
@@ -15,10 +43,14 @@ mod tests {
 
     #[rstest]
     #[case(
-        false,
+        AssetInfo {
+            family:String::from("cpython"),
+            version:String::from("3.10.9"),
+            tag:String::from("20230116")
+        },
         "cpython-3.10.9+20230116-aarch64-apple-darwin-debug-full.tar.zst"
     )]
-    fn test_from_asset_name(#[case] expected_result: bool, #[case] input: &str) {
-        assert_eq!(expected_result, AssetInfo::from_asset_name(input))
+    fn test_from_asset_name(#[case] expected_result: AssetInfo, #[case] input: &str) {
+        assert_eq!(Some(expected_result), AssetInfo::from_asset_name(input))
     }
 }
