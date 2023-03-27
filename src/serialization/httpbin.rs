fe30febdc886b4c55e5521fbdde671e009f9e9dc
Richard Cook <rcook@rcook.org>
2023-03-27 13:44:38 -0700

    Remove httpbin.rs

--- src/serialization/httpbin.rs
+++
@@ -1,7 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-pub struct HttpBinIPResponse {
-    #[serde(rename = "origin")]
-    pub origin: String,
-}
--- src/serialization/mod.rs
+++ src/serialization/mod.rs
@@ -1,8 +1,6 @@
 mod env;
 mod helpers;
-mod httpbin;
 mod index;
 
 pub use self::env::EnvRecord;
-pub use self::httpbin::HttpBinIPResponse;
 pub use self::index::{AssetRecord, PackageRecord};
