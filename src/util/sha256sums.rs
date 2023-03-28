b0e4d670f19005aa966e1c479518916a36531852
Richard Cook <rcook@rcook.org>
2023-03-28 09:11:28 -0700

    Create hash map

--- src/util/sha256sums.rs
+++ src/util/sha256sums.rs
@@ -1,6 +1,7 @@
 use crate::error::{fatal, Result};
 use crate::object_model::Tag;
 use include_dir::{include_dir, Dir};
+use std::collections::HashMap;
 
 static SHA256SUMS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/sha256sums");
 
@@ -12,6 +13,16 @@ pub fn dump_sha256sums(tag: &Tag) -> Result<()> {
     let s = f
         .contents_utf8()
         .ok_or(fatal("Resource could not be decoded at UTF-8"))?;
-    println!("s={:?}", s);
+
+    let mut map = HashMap::new();
+    for line in s.lines() {
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+        let checksum = parts[0];
+        let asset_name = parts[1];
+        _ = map.insert(asset_name, checksum);
+    }
+
+    println!("map={:#?}", map);
+
     Ok(())
 }
