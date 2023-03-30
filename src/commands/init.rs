1eeee6af61a1f29687a78416c0001b2691e460e7
Richard Cook <rcook@rcook.org>
2023-03-30 13:30:58 -0700

    Fix init and new commands

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -8,21 +8,25 @@ use std::fs::read_to_string;
 use std::path::PathBuf;
 
 pub fn do_init(app: &App) -> Result<()> {
-    let config_path = app.cwd.join(".isopy.yaml");
-    let s = read_to_string(&config_path)?;
+    let project_config_path = app.cwd.join(".isopy.yaml");
+    let s = read_to_string(&project_config_path)?;
     let project_record = serde_yaml::from_str::<ProjectRecord>(&s)?;
 
     let assets = app.read_assets()?;
     let asset = get_asset(&assets, &project_record.python_version, &project_record.tag)?;
 
     let archive_path = app.assets_dir.join(&asset.name);
-    let hex_digest = format!("{:x}", compute(path_to_str(&config_path)?));
+    if !archive_path.is_file() {
+        todo!("Download the archive automatically: for now, use the \"download\" command")
+    }
+
+    let hex_digest = format!("{:x}", compute(path_to_str(&project_config_path)?));
     let env_dir = app.dir.join("hashed").join(hex_digest);
     unpack_file(&archive_path, &env_dir)?;
 
     let env_path = env_dir.join("env.yaml");
     let env_record = AnonymousEnvRecord {
-        config_path: config_path,
+        config_path: project_config_path,
         python_dir: PathBuf::from("python"),
         python_version: asset.meta.version.clone(),
         tag: asset.tag.clone(),
--- src/commands/new.rs
+++ src/commands/new.rs
@@ -1,3 +1,4 @@
+use super::helpers::get_asset;
 use crate::app::App;
 use crate::error::{user, Result};
 use crate::object_model::{Tag, Version};
@@ -5,24 +6,28 @@ use crate::serialization::ProjectRecord;
 use crate::util::{is_already_exists, safe_write_to_file};
 
 pub fn do_new(app: &App, version: &Version, tag: &Option<Tag>) -> Result<()> {
-    let config_path = app.cwd.join(".isopy.yaml");
+    let project_config_path = app.cwd.join(".isopy.yaml");
     let project_record = ProjectRecord {
         python_version: version.clone(),
         tag: tag.clone(),
     };
 
+    // Make sure there's exactly one asset matching this Python version and tag
+    let assets = app.read_assets()?;
+    _ = get_asset(&assets, &project_record.python_version, &project_record.tag)?;
+
     let s = serde_yaml::to_string(&project_record)?;
-    match safe_write_to_file(&config_path, s, false) {
+    match safe_write_to_file(&project_config_path, s, false) {
         Ok(_) => {
             println!(
                 "Wrote project configuration file to {}",
-                config_path.display()
+                project_config_path.display()
             );
         }
         Err(e) if is_already_exists(&e) => {
             return Err(user(format!(
                 "Project configuration file {} already exists",
-                config_path.display()
+                project_config_path.display()
             )))
         }
         e => return e,
