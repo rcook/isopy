2300a5d47934d3baf7bcdc436982717921f13411
Richard Cook <rcook@rcook.org>
2023-04-02 09:52:25 -0700

    Move code around

--- src/commands/scratch.rs
+++ src/commands/scratch.rs
@@ -1,11 +1,10 @@
 use crate::app::App;
 use crate::helpers::get_asset;
 use crate::object_model::{LastModified, RepositoryName, Tag, Version};
-use crate::repository::{GitHubRepository, LocalRepository, Repository, Response};
+use crate::repository::{GitHubRepository, LocalRepository, Repository};
 use crate::result::Result;
 use crate::serialization::RepositoryRecord;
-use crate::util::{safe_create_file, ContentLength, Indicator};
-use std::io::Write;
+use crate::util::download_the_next_generation;
 use std::path::Path;
 
 fn make_repository(
@@ -37,7 +36,7 @@ pub async fn do_scratch(app: &App) -> Result<()> {
     for (repository_name, repository) in &enabled_repositories {
         let index_json_path = app.get_index_json_path(repository_name);
         let current_last_modified = app.read_index_last_modified(repository_name)?;
-        if let Some(new_last_modified) = test_repository(
+        if let Some(new_last_modified) = download_index(
             &current_last_modified,
             &index_json_path,
             repository.as_ref(),
@@ -56,12 +55,12 @@ pub async fn do_scratch(app: &App) -> Result<()> {
     )?;
     for (_repository_name, repository) in &enabled_repositories {
         let mut response = repository.get_asset(&asset).await?;
-        download("asset", &mut response, "foo.tar.gz").await?;
+        download_the_next_generation("asset", &mut response, "foo.tar.gz").await?;
     }
     Ok(())
 }
 
-async fn test_repository<P>(
+async fn download_index<P>(
     last_modified: &Option<LastModified>,
     index_json_path: P,
     repository: &dyn Repository,
@@ -71,29 +70,9 @@ where
 {
     let response_opt = repository.get_latest_index(last_modified).await?;
     if let Some(mut response) = response_opt {
-        download("index", &mut response, index_json_path).await?;
+        download_the_next_generation("index", &mut response, index_json_path).await?;
         Ok(Some(response.last_modified().clone()))
     } else {
         Ok(None)
     }
-}
-
-async fn download<P>(label: &str, response: &mut Box<dyn Response>, output_path: P) -> Result<()>
-where
-    P: AsRef<Path>,
-{
-    let indicator = Indicator::new(response.content_length())?;
-    let mut stream = response.bytes_stream()?;
-    let mut file = safe_create_file(output_path, true)?;
-    let mut downloaded = 0;
-    indicator.set_message(format!("Fetching {}", label));
-    while let Some(item) = stream.next().await {
-        let chunk = item?;
-        downloaded += chunk.len() as ContentLength;
-        file.write(&chunk)?;
-        indicator.set_position(downloaded);
-    }
-    indicator.set_message(format!("Finished fetching {}", label));
-    indicator.finish();
-    Ok(())
 }
--- src/util/download_file.rs
+++ src/util/download_file.rs
@@ -1,6 +1,7 @@
+use crate::repository::Response;
 use crate::result::Result;
 use crate::ui::ProgressIndicator;
-use crate::util::safe_create_file;
+use crate::util::{safe_create_file, ContentLength, Indicator};
 use futures_util::StreamExt;
 use reqwest::header::USER_AGENT;
 use reqwest::{Client, IntoUrl};
@@ -54,5 +55,29 @@ where
         output_path.as_ref().display()
     ));
 
+    Ok(())
+}
+
+pub async fn download_the_next_generation<P>(
+    label: &str,
+    response: &mut Box<dyn Response>,
+    output_path: P,
+) -> Result<()>
+where
+    P: AsRef<Path>,
+{
+    let indicator = Indicator::new(response.content_length())?;
+    let mut stream = response.bytes_stream()?;
+    let mut file = safe_create_file(output_path, true)?;
+    let mut downloaded = 0;
+    indicator.set_message(format!("Fetching {}", label));
+    while let Some(item) = stream.next().await {
+        let chunk = item?;
+        downloaded += chunk.len() as ContentLength;
+        file.write(&chunk)?;
+        indicator.set_position(downloaded);
+    }
+    indicator.set_message(format!("Finished fetching {}", label));
+    indicator.finish();
     Ok(())
 }
