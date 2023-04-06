6aa6c110bec3b5741cae7a076add509678d30cec
Richard Cook <rcook@rcook.org>
2023-04-05 19:13:48 -0700

    Map more errors

--- src/util/download_file.rs
+++ src/util/download_file.rs
@@ -1,5 +1,5 @@
 use crate::repository::Response;
-use crate::result::Result;
+use crate::result::{translate_io_error, Result};
 use crate::util::{safe_create_file, ContentLength, Indicator};
 use std::io::Write;
 use std::path::Path;
@@ -14,13 +14,14 @@ where
 {
     let indicator = Indicator::new(response.content_length())?;
     let mut stream = response.bytes_stream()?;
-    let mut file = safe_create_file(output_path, true)?;
+    let mut file = safe_create_file(&output_path, true)?;
     let mut downloaded = 0;
     indicator.set_message(format!("Fetching {}", label));
     while let Some(item) = stream.next().await {
         let chunk = item?;
         downloaded += chunk.len() as ContentLength;
-        file.write(&chunk)?;
+        file.write(&chunk)
+            .map_err(|e| translate_io_error(e, &output_path))?;
         indicator.set_position(downloaded);
     }
     indicator.set_message(format!("Finished fetching {}", label));
