bdefdb6247e6bd3571cd2fd4a1c9184824151fe3
Richard Cook <rcook@rcook.org>
2023-07-27 09:50:09 -0700

    Use unwrap_or_else

--- isopy/src/args.rs
+++ isopy/src/args.rs
@@ -40,7 +40,7 @@ const PACKAGE_BUILD_VERSION: Option<&str> = option_env!("RUST_TOOL_ACTION_BUILD_
     name = PACKAGE_NAME,
     version = PACKAGE_VERSION,
     about = format!("{} {}", PACKAGE_DESCRIPTION, PACKAGE_VERSION),
-    after_help = format!("{}\nhttps://github.com/rcook/isopy{}", PACKAGE_HOME_PAGE, PACKAGE_BUILD_VERSION.map(|x| format!("\n\n{}", x)).unwrap_or(String::from("")))
+    after_help = format!("{}\nhttps://github.com/rcook/isopy{}", PACKAGE_HOME_PAGE, PACKAGE_BUILD_VERSION.map(|x| format!("\n\n{}", x)).unwrap_or_else(|| String::from("")))
 )]
 pub struct Args {
     #[arg(
