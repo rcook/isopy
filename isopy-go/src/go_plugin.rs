777cf7df6e71fedb3ecf151f43ece713490d86fe
Richard Cook <rcook@rcook.org>
2023-08-18 10:47:19 -0700

    Implement Descriptor

--- isopy-go/src/go_plugin.rs
+++ isopy-go/src/go_plugin.rs
@@ -19,7 +19,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-use crate::constants::INDEX_FILE_NAME;
+use crate::constants::{ASSETS_DIR, INDEX_FILE_NAME};
 use crate::download::download_index;
 use crate::filter::Filter;
 use crate::go_version::GoVersion;
@@ -28,21 +28,25 @@ use async_trait::async_trait;
 use isopy_lib::{other_error, Descriptor, IsopyLibResult, Package, Plugin};
 use joatmon::read_yaml_file;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 pub struct GoPlugin {
     _offline: bool,
     dir: PathBuf,
+    assets_dir: PathBuf,
 }
 
 impl GoPlugin {
     pub fn new(offline: bool, dir: &Path) -> Self {
+        let assets_dir = dir.join(&*ASSETS_DIR);
         Self {
             _offline: offline,
             dir: dir.to_path_buf(),
+            assets_dir,
         }
     }
 
-    async fn get_available_versions(&self) -> IsopyLibResult<Vec<GoVersion>> {
+    async fn get_available_items(&self) -> IsopyLibResult<Vec<(Package, GoVersion)>> {
         let index_path = self.dir.join(&*INDEX_FILE_NAME);
 
         download_index(&Filter::default(), &index_path)
@@ -51,40 +55,50 @@ impl GoPlugin {
 
         let index_rec = read_yaml_file::<IndexRec>(&index_path).map_err(other_error)?;
 
-        let mut versions = Vec::new();
+        let mut items = Vec::new();
         for version_rec in index_rec.versions {
-            versions.push(
-                version_rec
-                    .version
-                    .parse::<GoVersion>()
-                    .map_err(other_error)?,
-            );
+            let asset_path = self.assets_dir.join(version_rec.file_name);
+            let version = version_rec
+                .version
+                .parse::<GoVersion>()
+                .map_err(other_error)?;
+
+            let descriptor: Arc<Box<dyn Descriptor>> = Arc::new(Box::new(version.clone()));
+
+            let package = Package {
+                asset_path,
+                descriptor,
+            };
+
+            items.push((package, version));
         }
 
-        versions.sort();
+        items.sort_by(|a, b| b.1.cmp(&a.1));
 
-        Ok(versions)
+        Ok(items)
     }
 }
 
 #[async_trait]
 impl Plugin for GoPlugin {
     async fn get_available_packages(&self) -> IsopyLibResult<Vec<Package>> {
-        let versions = self.get_available_versions().await?;
-        for version in versions {
-            println!("{}", version.raw);
-        }
-
-        Ok(vec![])
+        let items = self.get_available_items().await?;
+        Ok(items.into_iter().map(|p| p.0).collect::<Vec<_>>())
     }
 
     async fn get_downloaded_packages(&self) -> IsopyLibResult<Vec<Package>> {
-        let versions = self.get_available_versions().await?;
-        for version in versions {
-            println!("{}", version.raw);
+        let mut items: Vec<(Package, GoVersion)> = Vec::new();
+
+        if self.assets_dir.exists() {
+            let available_items = self.get_available_items().await?;
+            for item in available_items {
+                println!("{}", item.1.raw);
+            }
+            todo!();
         }
 
-        Ok(vec![])
+        items.sort_by(|a, b| b.1.cmp(&a.1));
+        Ok(items.into_iter().map(|p| p.0).collect::<Vec<_>>())
     }
 
     async fn download_package(&self, _descriptor: &dyn Descriptor) -> IsopyLibResult<Package> {
